b98e234c702c513ec83fa4a82a8739968e2edd52
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2023-02-10 03:00:13 +0300

    Update lw_5.R

--- lab1/lw_5.R
+++ lab1/lw_5.R
@@ -1,5 +1,11 @@
+# Напишите код, который заменяет случайно элементы некоторого вектора на пропущенные
+# значения (NA). Ваш код должен работать для вектора любой длины. Далее необходимо
+# вывести на экран индексы пропущенных значений в векторе. Напишите код, который
+# считает, сколько пропущенных значений в векторе.
+
 vec_size <- 10
 vec <- sample(1:100, vec_size, replace = F)
-random_replacemets_count <- sample(1:vec_size, 1)
-modified_vec <- replace(vec, sample(1:vec_size, random_replacemets_count), NA)
-which(modified_vec %in% NA)
+prob = runif(1, 0, 1)
+NA_vec <- sample(c(0, NA), vec_size, replace = T, prob=c(prob, 1 - prob))
+res_vec <- vec + NA_vec
+sum(res_vec %in% NA)
