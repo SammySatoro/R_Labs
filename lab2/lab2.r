a473decc2a80ec998c8f4c0307924faa2257f18d
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2023-02-17 23:46:02 +0300

    done

--- lab2/lab2.r
+++ lab2/lab2.r
@@ -1,11 +1,13 @@
 dataset <- read.csv('lab2/dataset.csv', sep=';', header=T, fileEncoding = 'cp1251')
 dataset[7, 3:12] <- c(2L, 4L, 1L, 6L, NA, 7L ,9L, 5L, 6L, 3L)
 dataset[4, 3:12] <- c(7L, 6L, 4L, 9L, 2L, 7L ,9L, 3L, 6L, 10L)
-mins <- apply(dataset[3:12], MARGIN = 2, min)
-maxes <- apply(dataset[3:12], MARGIN = 2, max)
-means <- sapply(dataset[3:12], MARGIN = 2, mean)
+mins <- apply(dataset[3:12], MARGIN = 2, min, na.rm=TRUE)
+maxes <- apply(dataset[3:12], MARGIN = 2, max, na.rm=TRUE)
+means <- sapply(dataset[3:12], MARGIN = 2, mean, na.rm=TRUE)
 mmm <- summary(dataset)[c(1,4,6),]
-
-`%notin%` <- Negate('%in%')
-
-decreasing_rating <- sort(means, decreasing = TRUE)
+not_in_range_of_3_7 <- apply((dataset[,3:12] < 3) + (dataset[,3:12] > 7), MARGIN = 2, sum, na.rm=TRUE);
+vectorized <- as.vector(not_in_range_of_3_7)
+decreasing_rating <- round(sort(means, decreasing = TRUE, na.last = TRUE), digits = 3)
+titles <- factor(rev(names(dataset[3:12])))
+ratings <- round(means, digits = 3)
+plot(titles, ratings, main='Rating Of Music', xlab="Title", ylab="Rating")
